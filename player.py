5ea5ce7271e4adf1b6267644c579e54ea20a7139
Howard Gill <theogill1369@gmail.com>
2022-08-17 14:30:20 -0400

    had to resave to repo

--- player.py
+++ player.py
@@ -1,6 +1,4 @@
 class Player:
-=======
-class Player:
     gestures = ["Rock ", "Paper","Scissors", "Lizard", "Spock"]
     def __init__(self):
         self.choose_number_of_players = input('Choose single player or multiplayer.')
