97898c5164167c09f498eee1d441130ed49790e0
Howard Gill <theogill1369@gmail.com>
2022-08-17 14:14:51 -0400

    Created most of the 'Outter Space' game area

--- outter_space.py
+++ outter_space.py
@@ -1,1 +1,43 @@
+# Explanation of game rules
+
+# Rock crushes Scissors
+
+# Scissors cuts Paper 
+
+# Paper covers Rock
+
+# Rock crushes Lizard
+
+# Lizard poisons Spock
+
+# Spock smashes Scissors
+
+# Scissors decapitates Lizard
+
+# Lizard eats Paper
+
+# Paper disproves Spock
+
+# Spock vaporizes Rock
+
+#  ask player to select opponent 
+
+# strart first round
+
+#  makes sure gesture and ties can be used
+
+# if yes display round winner
+
+#  continue to next round  (repeat steps 4-8)
+#   
+# if no rethrow (repeat steps 4-6)
+
+#  display game winner after 3 rounds
+
 class OutterSpace:
+
+    input("Welcome to Outter Space, where the ultimate battle of Rock, Paper, Scissor, Lizard, Spock!")
+
+    input("Here are the rules, you are not affriad of Spock's viporizing you!")
+
+    input("Rock crushes Scissors, Scissors cut paper")
