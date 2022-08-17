a76e3ca942a3d2e264e3f79881376f8de82074dc
Howard Gill <theogill1369@gmail.com>
2022-08-17 19:43:39 -0400

    Wrapped methods and variables inside functions in outter_spy.py file

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 from outter_space import OutterSpace
 
+print("start")
--- outter_space.py
+++ outter_space.py
@@ -2,8 +2,6 @@ from player import Player
 from human import Human
 from ai import Ai
 
-# Explanation of game rules
-
 # strart first round
 
 #  makes sure gesture and ties can be used
@@ -21,25 +19,36 @@ class OutterSpace:
     def __init__(self) -> None:
         self.human = Human(Player)
         self.ai = Ai(Player)
-    
-    def run_game(self):
-        self.display_welcome()
-        self.player_selection()
-        self.game_play()
-        self.display_winner()
-    
+
     def display_welcome(self):
         input("Welcome to Outter Space, where the ultimate battle of Rock, Paper, Scissor, Lizard, Spock!")
-
+        print()
+        print()
         input("Here are the rules, you are not affriad of Spock's viporizing you!")
-
+        print()
+        print()
         input("Rock crushes Scissors, Scissors cut paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock")
     
     def player_selection(self):
         single_player = "Single player"
 
         Multiplayer = "Multiplayer"
+
+        if user_input == Multiplayer:
+            pass
         
         user_input = input("Please select opponent: Single player or Multiplayer!")
 
+    def run_game(self):
+        self.display_welcome()
+        self.player_selection()
+        self.game_play()
+        self.display_winner()
+    
+    
+
     def game_play(self):
+        while 
+
+    def display_winner(self, winner):
+        return winner()
